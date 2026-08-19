005337758aaa2ea7e7eda1a2a310870431948e9f
Vasil <mdvasil07@gmail.com>
2026-08-19 17:42:28 +0530

    added the matplot -1 excercise

---
+++ Python advanced/Matplot/main.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+
+# plt.plot(x, y,color='red', marker='o', linestyle='--', linewidth=2, markersize=8)
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")
+# plt.title("Simple Plot")
+# plt.show()
+
+# plt.scatter(x, y, color='blue', marker='o', s=100)
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")
+# plt.title("Scatter Plot")
+# plt.show()
+
+# plt.bar(x, y, color='green', width=0.5)
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")
+# plt.title("Bar Chart")
+# plt.show()
+
+# marks = [32, 45, 48, 51, 55, 56, 62, 65, 67, 70, 72, 75, 80, 85, 90]
+# plt.hist(marks, bins=3)
+
+# plt.xlabel("Marks")
+# plt.ylabel("Number of Students")
+# plt.title("Distribution of Marks")
+
+# plt.show()
+
+# sizes=[15, 30, 45, 10]
+# labels=['xxl', 'xl', 'l', 'm']
+# plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+# plt.title("Pie Chart")
+# plt.show()
